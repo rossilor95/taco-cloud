e8d063dc15d73b90ba994593700dd9aff1d24c00
rossilor95 <rossilor95@outlook.it>
2023-03-03 14:54:15 +0100

    feat(Chapter2): add OrderController

---
+++ src/main/java/tacos/web/OrderController.java
@@ -0,0 +1,34 @@
+package tacos.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import tacos.TacoOrder;
+
+/**
+ * This controller handles requests for <code>/orders/current</code>. After submitting a taco creation from
+ * <code>/design</code>, the user is redirected to an order form from which they can place an order to have their taco
+ * delivered.
+ */
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
+public class OrderController {
+    private static final Logger log = LoggerFactory.getLogger(DesignTacoController.class);
+
+    @GetMapping("/current")
+    public String showOrderForm() {
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+        log.info("Order submitted: {}", order);
+        return "redirect:/";
+    }
+}
