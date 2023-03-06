cd3b069ef50b2189601b054c877ed4c55b36152a
rossilor95 <rossilor95@outlook.it>
2023-03-06 23:23:35 +0100

    chore(Chapter2): add validation annotations to Taco and TacoOrder

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -1,16 +1,37 @@
 package tacos;
 
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.CreditCardNumber;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class TacoOrder {
+
+    @NotBlank(message = "City is required")
     private String deliveryCity;
+
+    @NotBlank(message = "Street is required")
     private String deliveryStreet;
+
+    @NotBlank(message = "Delivery name is required")
     private String deliveryName;
+
+    @NotBlank(message = "State is required")
     private String deliveryState;
+
+    @NotBlank(message = "Zip code is required")
     private String deliveryZip;
+
+    @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
+
+    @DateTimeFormat(pattern = "MM/YY")
     private String ccExpiration;
+
+    @Digits(integer = 3, fraction = 0, message = "Invalid CCV")
     private String ccCVV;
 
     private List<Taco> tacos = new ArrayList<>();
