d9f9a19eb64999cc5bde1e672add1fbe44aba291
rossilor95 <rossilor95@outlook.it>
2023-03-07 18:50:05 +0100

    fix(Chapter2): fix ccExpiration validation in TacoOrder

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -2,8 +2,8 @@ package tacos;
 
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.CreditCardNumber;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,7 +28,7 @@ public class TacoOrder {
     @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
 
-    @DateTimeFormat(pattern = "MM/YY")
+    @Pattern(regexp = "^(0[1-9]|1[0-2])/([2-9][4-9])", message = "Must be formatted MM/YY")
     private String ccExpiration;
 
     @Digits(integer = 3, fraction = 0, message = "Invalid CCV")
