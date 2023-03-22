d0ec020bc8f7c35e379b59bd9987959ff35ae37e
rossilor95 <rossilor95@outlook.it>
2023-03-22 18:24:15 +0100

    feat(Chapter 3): make TacoOrder implement Serializable

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -5,35 +5,32 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-public class TacoOrder {
+public class TacoOrder implements Serializable {
 
+    private static final long serialVersionUid = 1L;
+    private long id;
+    private Date placedAt;
     @NotBlank(message = "City is required")
     private String deliveryCity;
-
     @NotBlank(message = "Street is required")
     private String deliveryStreet;
-
     @NotBlank(message = "Delivery name is required")
     private String deliveryName;
-
     @NotBlank(message = "State is required")
     private String deliveryState;
-
     @NotBlank(message = "Zip code is required")
     private String deliveryZip;
-
     @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
-
     @Pattern(regexp = "^(0[1-9]|1[0-2])/([2-9][4-9])", message = "Must be formatted MM/YY")
     private String ccExpiration;
-
     @Digits(integer = 3, fraction = 0, message = "Invalid CCV")
     private String ccCVV;
-
     private List<Taco> tacos = new ArrayList<>();
 
     public TacoOrder() {
@@ -59,6 +56,22 @@ public class TacoOrder {
         this.ccExpiration = ccExpiration;
         this.ccCVV = ccCVV;
         this.tacos = tacos;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getPlacedAt() {
+        return placedAt;
+    }
+
+    public void setPlacedAt(Date placedAt) {
+        this.placedAt = placedAt;
     }
 
     public void addTaco(Taco taco) {
