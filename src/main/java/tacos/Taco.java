ff9d9f8da06536c720f59cd5566cb260ab1457be
rossilor95 <rossilor95@outlook.it>
2023-03-22 18:13:42 +0100

    feat(Chapter 3): add ID and timestamp to Taco

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -3,17 +3,22 @@ package tacos;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+import java.util.Date;
 import java.util.List;
 
 public class Taco {
 
     @NotNull
-    @Size(min=5, message = "Name must be at least 5 characters long")
+    @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
     @NotNull
     @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
+
+    private Long id;
+
+    private Date createdAt;
 
     public Taco() {
     }
@@ -21,6 +26,22 @@ public class Taco {
     public Taco(String name, List<Ingredient> ingredients) {
         this.name = name;
         this.ingredients = ingredients;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
     }
 
     public String getName() {
