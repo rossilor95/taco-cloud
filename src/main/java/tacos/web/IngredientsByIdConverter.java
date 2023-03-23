d6e37b474b9d004eadf390496e4147626a22d1e6
rossilor95 <rossilor95@outlook.it>
2023-03-23 19:04:24 +0100

    feat(Chapter 3): simplify IngredientsByIdConverter with ingredientRepo

--- src/main/java/tacos/web/IngredientsByIdConverter.java
+++ src/main/java/tacos/web/IngredientsByIdConverter.java
@@ -1,10 +1,11 @@
 package tacos.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import tacos.Ingredient;
-
-import java.util.Map;
+import tacos.data.IngredientRepository;
 
 /**
  * This class automatically converts ingredients string ids returned from the <code>/design</code> template view into
@@ -12,21 +13,15 @@ import java.util.Map;
  */
 @Component
 public class IngredientsByIdConverter implements Converter<String, Ingredient> {
-    private final Map<String, Ingredient> ingredientMap = Map.of(
-            "FLTO", new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
-            "COTO", new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
-            "GRBF", new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
-            "CARN", new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
-            "TMTO", new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
-            "LETC", new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
-            "CHED", new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
-            "JACK", new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
-            "SLSA", new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
-            "SRCR", new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
-    );
+    private final IngredientRepository ingredientRepository;
+
+    @Autowired
+    public IngredientsByIdConverter(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
 
     @Override
-    public Ingredient convert(String id) {
-        return ingredientMap.get(id);
+    public Ingredient convert(@NonNull String id) {
+        return ingredientRepository.findById(id).orElseThrow();
     }
 }
