cbc77147dfc2c0ff1465c0e86a0d1c534e0d40bc
rossilor95 <rossilor95@outlook.it>
2023-03-23 19:05:14 +0100

    feat(Chapter 3): update and improve IngredientsByIdConverterTest

--- src/test/java/tacos/web/IngredientsByIdConverterTest.java
+++ src/test/java/tacos/web/IngredientsByIdConverterTest.java
@@ -2,24 +2,31 @@ package tacos.web;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import tacos.Ingredient;
+
+import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class IngredientsByIdConverterTest {
+
+    @Autowired
+    IngredientsByIdConverter converter;
 
     @Test
     @DisplayName("Should convert known ID")
     void shouldConvertKnownId() {
-        IngredientsByIdConverter converter = new IngredientsByIdConverter();
         Ingredient ingredient = new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP);
         assertEquals(ingredient, converter.convert("FLTO"));
     }
 
     @Test
-    @DisplayName("Should not convert unknown ID")
-    void shouldNotConvertUnknownId() {
-        IngredientsByIdConverter converter = new IngredientsByIdConverter();
-        assertNull(converter.convert("XXXX"));
+    @DisplayName("Should throw when trying to convert non-existent ID")
+    void shouldThrowWhenTryingToConvertNonExistentId() {
+        Exception e = assertThrows(NoSuchElementException.class, () -> converter.convert("XXXX"));
+        assertTrue(e.getMessage().contains("No value present"));
     }
 }
